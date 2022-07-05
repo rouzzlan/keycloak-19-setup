1b9ced584500675142578f3939848d76637d037d
Rouslan Khayaouri <rouslan_kh@hotmail.com>
2022-07-06 00:42:43 +0200

    additional script for rocky linux with mysql + keycloak

---
+++ keycloak-launch-2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if ! [ "$USER" = root ]; then
+  echo run this script with sudo
+  exit 3
+fi
+
+export KEYCLOAK_ADMIN=rouslan
+export KEYCLOAK_ADMIN_PASSWORD='50m9FiD3'
+export KC_HOSTNAME='keycloak-dev-1.localdomain'
+export KC_HTTPS_CERTIFICATE_KEY_FILE=/home/rouslan/keycloak-18.0.2/bin/certs/ca-key.pem
+export KC_HTTPS_CERTIFICATE_FILE=/home/rouslan/keycloak-18.0.2/bin/certs/ca-cert.pem
+export KC_DB_PASSWORD='rte.uY694a8bNrW-MQ_mBz'
+export KC_DB_URL='jdbc:mysql://192.168.2.21:3306/keycloak_dev_db_1'
+export KC_DB_USERNAME=test_user
+export KC_DB=mysql
+
+sh kc.sh start
